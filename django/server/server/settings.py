497f591e9a9a11a8b79e2cb946732f52e0c5e7c6
Baiyi Zhang <100baiyi@gmail.com>
2024-04-07 01:34:08 -0400

    config project db

--- django/server/server/settings.py
+++ django/server/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "movieRetrievalSystem.apps.MovieRetrievalSystemConfig"
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,8 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'movie.db',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
