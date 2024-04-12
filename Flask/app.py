d4d4aaf084b00185105260011b8c879f91ff26be
MicahHarlan <81989762+MicahHarlan@users.noreply.github.com>
2024-04-12 12:08:46 -0400

    cohere LLM

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,jsonify,request
 import llm
-generator = llm.init_generator()
+#generator = llm.init_generator()
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def llm_response():
 	data = request.get_json()
 	user_input = data.get('userInput')
     # Process the input and generate a response
-	response_message = llm.get_llm(user_input,generator)
+	response_message = llm.get_llm(user_input)
 	return jsonify({'message': response_message})
 
 @app.route('/')
--- Flask/llm.py
+++ Flask/llm.py
@@ -1,20 +1,26 @@
-from transformers import pipeline, set_seed
-
+import cohere
+co = cohere.Client('vm4DXIEQpEBN9YFD19VTnxYWTRCeHNyge71iMWgk')
 
 '''
 The tokenizer or generators should run in this function and set to a var
 in the flask app, so it only runs once.
 '''
-def init_generator():
-	generator = pipeline('text-generation', model='gpt2')
-	return generator
+#def init_generator():
+	#	generator = pipeline('text-generation', model='gpt2')
+	#return generator
 
 '''
 Make sure this returns a string.
 '''
-def get_llm(prompt,generator):
-	responses = generator(prompt, max_length=50, num_return_sequences=1)
-	return responses[0]['generated_text'] 
-	# Generate
 
+#def get_llm(prompt,generator):
+#	responses = generator(prompt, max_length=50, num_return_sequences=1)
+#	return responses[0]['generated_text'] 
+#	# Generate
+
+def get_llm(prompt):
+	response = co.generate(
+  	prompt=prompt
+	)
+	return response.generations[0].text
 
