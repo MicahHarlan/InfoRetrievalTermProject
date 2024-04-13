444cd540921a0fd6cb50724722d4818c0ff5bb76
Baiyi Zhang <100baiyi@gmail.com>
2024-04-13 01:22:14 -0400

    add test for fastapi calling

--- Flask/app.py
+++ Flask/app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template,jsonify,request
 import llm
 #generator = llm.init_generator()
@@ -11,12 +12,21 @@ def llm_response():
 	user_input = data.get('userInput')
     # Process the input and generate a response
 	response_message,titles = llm.get_llm(user_input)
+
+	# Testing fastapi output
+	print(send_to_fastapi("Forrest Gump"))
 	
 	return jsonify({'message': response_message,'titles':titles})
 
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+def send_to_fastapi(query):
+    url = 'http://127.0.0.1:8000/search'
+    params = {'q': query}
+    response = requests.get(url, params=params)
+    return response.json()
 
 if __name__ == '__main__':
 	#For running on local host.
